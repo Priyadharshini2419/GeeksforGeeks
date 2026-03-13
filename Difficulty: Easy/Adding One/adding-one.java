a3b5120d4a2cdc292e9bbf45eff6f4f78d17ff10
Priyadharshini2419 <dharshinisp2005@gmail.com>
2026-03-13 10:25:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Adding One/adding-one.java
+++ Difficulty: Easy/Adding One/adding-one.java
@@ -1,28 +1,25 @@
-import java.util.*;
+// User function Template for Java
 
 class Solution {
+    // Function for adding one to the number represented by the array
     Vector<Integer> addOne(int[] arr) {
-        int n = arr.length;
-
-        for(int i = n - 1; i >= 0; i--){
-            if(arr[i] < 9){
+        int n=arr.length;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]<9){
                 arr[i]++;
                 break;
-            }else{
-                arr[i] = 0;
+            }
+            else{
+                arr[i]=0;
             }
         }
-
-        Vector<Integer> ans = new Vector<>();
-
-        if(arr[0] == 0){  
-            ans.add(1);
+        Vector<Integer>r=new Vector<>();
+        if(arr[0]==0){
+            r.add(1);
         }
-
-        for(int x : arr){
-            ans.add(x);
+        for(int x:arr){
+            r.add(x);
         }
-
-        return ans;
+        return r;
     }
 }
